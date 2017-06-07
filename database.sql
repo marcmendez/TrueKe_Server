da7072b4f4e41bb9c7feb7096a62aaafb22964db
Diego Lao Tebar <howarto@ymail.com>
2017-06-07 14:52:22 +0200

    Fix encoding issue

--- database.sql
+++ database.sql
@@ -2,7 +2,7 @@
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `restful_api` DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+CREATE SCHEMA IF NOT EXISTS `restful_api` CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `restful_api`;
 -- -----------------------------------------------------
 -- Table `restful_api`.`user`
